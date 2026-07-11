bb44b5bb410a00adf44852648e24e8608bda5689
Gadget Academy <gadgetacademy63@gmail.com>
2026-07-11 14:44:41 +0530

    refactor: Export lexer module from lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub mod ast;
 pub mod compiler;
+pub mod lexer;
 pub mod runtime;
 pub mod stdlib;
 
 pub use ast::{Expression, Statement, TypeNode, VarDecl};
 pub use compiler::Compiler;
+pub use lexer::{Lexer, Token, TokenType};
